3e88d7a27bea42a5343e7393710e54b3745f439a
RajputAtul75 <skdjca803@gmail.com>
2026-01-03 14:39:33 +0530

    done

---
+++ 2.Conditional_Statement/1_Exa.dart
@@ -0,0 +1,25 @@
+// use of if and else
+void main(){
+
+  int age = 18;
+  if(age >= 18){
+    print("Adult");
+  }
+  else{
+    print("Kids");
+  }
+  // multiple if else statement
+  int a = 10;
+  int b = 15;
+  int c = a+b;
+
+  if(c > b){
+    print("c is bigger than a");
+  }
+  if(a > b){
+    print("a is bigger than b");
+  }
+  else{
+    print("b is bigger than a");
+  }
+}
---
+++ 2.Conditional_Statement/2_Using_opera.dart
@@ -0,0 +1,11 @@
+//import 'dart:io';
+void main(){
+  // taking input
+  //int marks = int.parse(stdin.readLineSync()!);
+  //print(marks);
+  int marks = 100;
+
+  if(marks >= 40 && marks >= 50){
+    print("Pass.");
+  }
+}
