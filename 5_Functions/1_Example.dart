c616270cfae2ada667c5cd1bb5341119a542b4c4
RajputAtul75 <skdjca803@gmail.com>
2026-01-21 21:15:56 +0530

    done

---
+++ 5_Functions/1_Example.dart
@@ -0,0 +1,13 @@
+void main(){
+  //Function call
+  printName();
+  int a = age();
+  print('Age is:${a}');
+}
+//Functions definitions
+void printName(){
+  print('Atul Singh');
+}
+int age(){
+  return 21;
+}
